fee11e8af3e20921825b680d8230cd965ed4502c
Thiimo <thimosietsma@gmail.com>
2023-01-26 15:59:30 +0100

    logging

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import uploader from './uploader'
 
 const app = express();
 
-app.use(morgan("common"));
+app.use(morgan("combined"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "100mb"}));
